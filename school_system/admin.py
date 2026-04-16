88a2f1afef28cda6ae4ff089578a576d2057d1f2
adhiexee <137257063+adhi1105@users.noreply.github.com>
2026-04-16 17:20:03 +0530

    Enable high-end bulk password reset for admins in the Django panel

--- school_system/admin.py
+++ school_system/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from unfold.contrib.auth.admin import UserAdmin as UnfoldUserAdmin
+from django.contrib.auth.forms import PasswordResetForm
+from django.utils.translation import gettext_lazy as _
+from unfold.decorators import action
 from .models import Profile, Stream, Subject
 
 # --- 1. USER & PROFILE MANAGEMENT ---
@@ -11,7 +14,7 @@ class ProfileInline(admin.StackedInline):
     verbose_name_plural = 'Role & Profile'
     fk_name = 'user'
 
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(UnfoldUserAdmin):
     inlines = (ProfileInline,)
 
     def get_inline_instances(self, request, obj=None):
@@ -21,12 +24,33 @@ class UserAdmin(BaseUserAdmin):
         return super().get_inline_instances(request, obj)
 
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_role')
+    actions = ['trigger_password_reset_bulk']
     
     def get_role(self, instance):
         if hasattr(instance, 'profile'):
             return instance.profile.role
         return '-'
     get_role.short_description = 'Role'
+
+    @action(description=_("Trigger Secure Password Reset"), icon="mail")
+    def trigger_password_reset_bulk(self, request, queryset):
+        """
+        Sends password reset emails to the selected users using the branded template.
+        """
+        sent_count = 0
+        for user in queryset:
+            if user.email:
+                form = PasswordResetForm({'email': user.email})
+                if form.is_valid():
+                    form.save(
+                        request=request,
+                        use_https=request.is_secure(),
+                        email_template_name='auth/password_reset_email.html',
+                        subject_template_name='auth/password_reset_subject.txt',
+                    )
+                    sent_count += 1
+        
+        self.message_user(request, f"Successfully dispatched password reset emails to {sent_count} users.")
 
 # Re-register User to include Profile inlines
 admin.site.unregister(User)
